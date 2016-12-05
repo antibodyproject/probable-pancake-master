6217845e2679850c6ff227f5c79aab05b890c55c
antibodyproject <hensonb17@mail.wlu.edu>
2016-12-04 19:06:34 -0500

    12.4Lyn1Done

---
+++ Antibody 2/Assets/Scripts/Coulomb/CoulombManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CoulombManager : MonoBehaviour {
+    private float cycleInterval = 0.01f;
+
+    private List<ChargedParticle> chargedParticles;
+    private List<MovingChargedParticle> movingChargedParticles;
+
+
+
+    void Start() {
+        chargedParticles = new List<ChargedParticle> (FindObjectsOfType<ChargedParticle> ());
+        movingChargedParticles = new List<MovingChargedParticle>(FindObjectsOfType<MovingChargedParticle> ());
+
+
+        foreach (MovingChargedParticle mcp in movingChargedParticles)
+            StartCoroutine (Cycle (mcp));
+
+
+    }
+
+    public IEnumerator Cycle(MovingChargedParticle mcp){
+        bool isFirst = true;
+
+        while (true) {
+
+            if (isFirst){
+                isFirst=false;
+                yield return new WaitForSeconds(Random.Range (0,cycleInterval));
+            }
+
+            ApplyMagneticForce(mcp);
+            yield return new WaitForSeconds(cycleInterval);             
+        }
+    }
+                 
+    private void ApplyMagneticForce(MovingChargedParticle mcp){
+        Vector3 newForce = Vector3.zero;
+        
+        foreach (ChargedParticle cp in chargedParticles) {
+            if (mcp == cp)
+                continue;
+
+            float distance=Vector3.Distance(mcp.transform.position,cp.gameObject.transform.position);
+            float force = 1000 * mcp.charge * cp.charge / Mathf.Pow(distance, 2);
+
+            Vector3 direction = mcp.transform.position-cp.transform.position;
+            direction.Normalize();
+
+            newForce += force * direction * cycleInterval;
+
+            if (float.IsNaN(newForce.x))
+                newForce = Vector3.zero;
+
+            mcp.rb.AddForce(newForce);
+        }
+    }
+}
---
+++ Antibody 2/Assets/Scripts/TagStick.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+//attach this script to the gameobject that you want to throw
+
+//the collider of the target must be a trigger
+
+//drag the gameobject in to this public variable
+
+var rb : Rigidbody;
+
+//type the tag of the target gameobject here
+
+var objectTag : String;
+
+function Start() {
+    rb = GetComponent.<Rigidbody>();
+}
+
+function OnTriggerEnter (other : Collider) {
+    if(other.gameObject.tag == objectTag){
+        rb.isKinematic = true;
+    }
+}
