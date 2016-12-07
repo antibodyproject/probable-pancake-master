e524efc78440badfd393a3c4115c4a438c7e5aad
antibodyproject <hensonb17@mail.wlu.edu>
2016-12-07 17:23:40 -0500

    introin

---
+++ Antibody 2/Assets/WaitPSO.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+
+[RequireComponent(typeof(AudioSource))]
+
+public class WaitPSO : MonoBehaviour
+
+{
+    public float delay;
+    public AudioClip otherClip;
+
+
+
+
+    IEnumerator Start()
+
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+
+        yield return new WaitForSeconds(delay);
+        audio.Play();
+
+
+
+    }
+
+}
