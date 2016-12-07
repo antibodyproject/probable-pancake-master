7173d9b621c744393cdffa777a0948c01aaca3d0
antibodyproject <hensonb17@mail.wlu.edu>
2016-12-06 23:52:17 -0500

    12.06

---
+++ Antibody 2/Assets/Scripts/Tags/TagWaitOn.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var thing : GameObject;
+var objectTag: String;
+var duration : float = 1;
+
+function Start () {
+    thing.SetActive(false);
+}
+
+function OnTriggerEnter (other : Collider) {
+    if(other.gameObject.tag == objectTag){
+        yield WaitForSeconds(duration);
+        thing.SetActive(true);
+    }
+}
